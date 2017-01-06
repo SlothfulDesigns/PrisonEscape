0775773c2833ed34680cbb8d627f05dc8e19127f
Niko Salakka <nsalakka@gmail.com>
2017-01-07 00:26:43 +0200

    weapon follows camera rotation and added new weapon sway

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,6 +6,14 @@ public class Weapon : MonoBehaviour {
 
     private PlayerController _player;
 
+    private Vector3 _bobMidpoint;
+    private float _bobSpeed = 0.2f;
+    private float _bobAmountX = 0.002f;
+    private float _bobAmountY = 0.005f;
+    private float _bobAmountZ = 0.001f;
+    private float _bobTimer = 0.0f;
+    private float _bobWave = 0.0f;
+
     private AudioSource _audioSource;
     private Camera _camera;
     private LineRenderer _lineRenderer;
@@ -22,6 +30,9 @@ public class Weapon : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         _camera = Camera.main;
+        _bobMidpoint = this.transform.localPosition;
+
+
         _audioSource = this.GetComponent<AudioSource>();
 
         _barrel = GameObject.Find("Barrel");
@@ -37,8 +48,44 @@ public class Weapon : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        WeaponSway();
 	}
+
+    private void WeaponSway()
+    {
+        var h = Mathf.Abs(Input.GetAxis("Horizontal"));
+        var v = Mathf.Abs(Input.GetAxis("Vertical"));
+
+        if (h == 0.0f && v == 0.0f)
+        {
+            _bobTimer = 0.0f;
+        }
+        else
+        {
+            _bobWave = Mathf.Sin(_bobTimer);
+            _bobTimer = _bobTimer + _bobSpeed;
+            if (_bobTimer > Mathf.PI * 2.0f)
+            {
+                _bobTimer = _bobTimer - (Mathf.PI * 2.0f);
+            }
+        }
+
+        if (_bobWave != 0.0f)
+        {
+            var axes = Mathf.Clamp(h + v, 0.0f, 1.0f);
+            var swayX = (_bobWave * _bobAmountX * axes);
+            var swayY = (_bobWave * _bobAmountY * axes);
+            var swayZ = (_bobWave * _bobAmountZ * axes);
+            var offset = new Vector3(swayX, swayY, swayZ);
+            this.transform.localPosition += offset;
+        }
+        else
+        {
+            this.transform.localPosition = _bobMidpoint;
+        }
+    }
 
     public void Shoot()
     {
