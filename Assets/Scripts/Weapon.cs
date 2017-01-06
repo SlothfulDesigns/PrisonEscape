5ce41124854ac80fa2ee0946d6bd303fa5388ec3
Niko Salakka <nsalakka@gmail.com>
2017-01-06 20:45:42 +0200

    CANNONING

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,14 +6,27 @@ public class Weapon : MonoBehaviour {
     public int damage = 100;
 
     private AudioSource _audioSource;
-    private AudioClip _shootFx;
     private Camera _camera;
+    private LineRenderer _lineRenderer;
+
+    private AudioClip _shotSfx;
+    private GameObject _shotVfx;
+    private GameObject _hitVfx;
+
+    private GameObject _barrel;
 
 	// Use this for initialization
 	void Start () {
         _camera = Camera.main;
         _audioSource = this.GetComponent<AudioSource>();
-        _shootFx = Resources.Load<AudioClip>("SHOT");
+
+        _barrel = GameObject.Find("Barrel");
+
+        _shotSfx = Resources.Load<AudioClip>("SHOT");
+        _shotVfx = Resources.Load<GameObject>("Smoke");
+        _hitVfx = Resources.Load<GameObject>("Flare");
+
+        _lineRenderer = this.GetComponent<LineRenderer>();
 	}
 	
 	// Update is called once per frame
@@ -23,18 +36,25 @@ public class Weapon : MonoBehaviour {
 
     public void Shoot()
     {
-        _audioSource.PlayOneShot(_shootFx);
+        _audioSource.PlayOneShot(_shotSfx);
+        var vfx = Instantiate(_shotVfx, _barrel.transform.position, _barrel.transform.rotation);
+        vfx.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
 
-        var rc = new Ray(_camera.transform.position, _camera.transform.forward);
+        Destroy(vfx, 1.0f);
+        var rc = new Ray(_barrel.transform.position, _camera.transform.forward);
         var hit = new RaycastHit();
+
         if(Physics.Raycast(rc, out hit))
         {
+
             var enemy = hit.collider.GetComponentInParent<Enemy>();
             if (enemy != null)
             {
                 Debug.Log("Hit " + enemy.name + " with " + this.name + " for " + this.damage + " damage.");
                 enemy.Damage(this.damage);
             }
+            var hitVfx = Instantiate(_hitVfx, hit.point, Quaternion.identity);
+            Destroy(hitVfx, 0.1f);
         }
     }
 }
